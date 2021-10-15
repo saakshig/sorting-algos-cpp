5ec359a0000705333ae910c56e391a92d2c6f7b4
Saakshi Gupta <37900226+saakshig@users.noreply.github.com>
2021-10-15 09:09:19 +0530

    Create LC142.cpp

---
+++ Patterns/Two Pointers/LC142.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        int cycleLength = 0;
+        while(fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow) {
+                do {
+                    fast = fast->next;
+                    cycleLength++;
+                } while(fast != slow);
+                break;
+            }
+        }
+
+        if(cycleLength == 0)
+            return NULL;
+
+        fast = head;
+        slow = head;
+        while(cycleLength > 0) {
+            fast = fast->next;
+            cycleLength--;
+        }
+
+        while(fast != slow) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        return slow;
+    }
+};
