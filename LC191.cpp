23ad69bf626d98d9c69712377cc2bc1610849737
Saakshi Gupta <37900226+saakshig@users.noreply.github.com>
2022-02-12 21:28:12 +0530

    Update LC191.cpp

--- LC191.cpp
+++ LC191.cpp
@@ -3,10 +3,17 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n != 0){
+            /*
+            //Approach 1 : Right shifting and anding with 1 to see if the last bit is 1
             if(n & 1){
                 count++;
             }
             n = n >> 1;
+            */
+            
+            //Approach 2 : n & n-1
+            count++;
+            n = n & (n-1);
         }
         return count;
     }
